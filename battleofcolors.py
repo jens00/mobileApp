5d31e6474754008d48d82d1874b7ab8e049b3713
jens00 <jensdom01@live.be>
2024-10-12 21:12:17 +0200

    Update battleofcolors.py

--- battleofcolors.py
+++ battleofcolors.py
@@ -16,6 +16,12 @@ def addPlayer(client_id: str)
   if len(queue) > 0:
     random_player = random.choice(queue)
     queue.remove(random_player)
+
+    try:
+      await websocket.send_text("found player")
+      await asyncio.wait_for(websocket.receive_text(), timeout=10)
+    except (asyncio.TimeoutError):
+      print("No response from client: " + client_id)
   else:
     queue.append(client_id)
     
