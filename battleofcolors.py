3a65c0c8eec38106b26b37166b3baf9ae051c793
jens00 <jensdom01@live.be>
2024-10-15 13:44:33 +0200

    Update battleofcolors.py

--- battleofcolors.py
+++ battleofcolors.py
@@ -20,9 +20,9 @@ async def addPlayer(client_id: str, clients: Dict[str, WebSocket]):
     websocket = clients.get(random_player)
 
     if websocket is None:
-        print("Error: WebSocket is not initialized.")
-        players.remove(client_id)
-        return
+      print("Error: WebSocket is not initialized.")
+      players.remove(client_id)
+      return
     
     queue.remove(random_player)
 
@@ -39,8 +39,17 @@ async def addPlayer(client_id: str, clients: Dict[str, WebSocket]):
   else:
     queue.append(client_id)
 
+async def endTurn(client_id: str, websocket: WebSocket):
+  if websocket is None:
+    print("Error: WebSocket is not initialized.")
+    
+  try:
+    await websocket.send_text(f"turn:boc:{client_id}")
+  except Exception:
+    pass
+
 def removePlayerById(client_id: str):
   if client_id in players:
-      players.remove(client_id)
+    players.remove(client_id)
   if client_id in queue:
-      queue.remove(client_id)
+    queue.remove(client_id)
